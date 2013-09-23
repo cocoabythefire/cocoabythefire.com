5edb1b31ca1eaf93d307b03a6186b329415fb2a0
Brittany Tarvin <bntarvin@fadingred.com>
2013-09-23 14:11:07 -0300

    more work on parallax

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -1,11 +1,15 @@
 $(function () {
+  var element = $('body#static_pages.index section.header');
+  var bottom = element.offset().top + element.height();
   $(document).scroll(function(event) {
-    var element = $('body#static_pages.index section.header');
     var scroll = $(document).scrollTop();
-    var bottom = element.offset().top + element.height();
-    var percentVisible = (bottom - scroll) / bottom;
-    // y begins at 50% and increases a bit based on space available
-    var yOffset = (50 + 20 * (1 - percentVisible)) + '%';
-    element.css({'background-position': '50% ' + yOffset});
+    if (scroll <= bottom) {
+      var percentVisible = (bottom - scroll) / bottom;
+      // y begins at 50% and increases a bit based on space available
+      var yOffsetInitial = 50;
+      var yOffsetDelta = 30;
+      var yOffset = (yOffsetInitial + yOffsetDelta * (1 - percentVisible)) + '%';
+      element.css({'background-position': '50% ' + yOffset});
+    }
   });
 });

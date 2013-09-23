0c40e3bb5bbd98d45612e5764ab160a6dda1b657
Brittany Tarvin <bntarvin@fadingred.com>
2013-09-23 14:40:08 -0300

    progress on the sticky navigation

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -1,15 +1,25 @@
 $(function () {
-  var element = $('body#static_pages.index section.header');
-  var bottom = element.offset().top + element.height();
+  var header = $('body#static_pages.index section.header');
+  var nav = $('body#static_pages.index nav');
+  var header_bottom = header.offset().top + header.height();
+  var nav_top = nav.offset().top;
   $(document).scroll(function(event) {
     var scroll = $(document).scrollTop();
-    if (scroll <= bottom) {
-      var percentVisible = (bottom - scroll) / bottom;
+    if (scroll <= header_bottom) {
+      var percentVisible = (header_bottom - scroll) / header_bottom;
       // y begins at 50% and increases a bit based on space available
       var yOffsetInitial = 50;
       var yOffsetDelta = 30;
       var yOffset = (yOffsetInitial + yOffsetDelta * (1 - percentVisible)) + '%';
-      element.css({'background-position': '50% ' + yOffset});
+      header.css({'background-position': '50% ' + yOffset});
+    }
+
+    if (scroll > nav_top && !nav.hasClass('sticky')) {
+      // console.log('yay! hello lovie');
+      nav.addClass('sticky');
+    }
+    else if (scroll <= nav_top && nav.hasClass('sticky')) {
+      nav.removeClass('sticky');
     }
   });
 });

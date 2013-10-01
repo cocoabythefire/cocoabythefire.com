1deddb782d7541c75ffd8757fc3fc6cde46af17a
Brittany Tarvin <bntarvin@fadingred.com>
2013-10-01 14:11:35 -0300

    added google analytics tracker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 CocoaByTheFire::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  GA.tracker = "UA-44507334-1"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
